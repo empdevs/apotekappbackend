52ce8f14f185189ac66b1aef65fc6fcefbea4201
haekalarif <haekal.rozikin@kitameraki.com>
2022-04-17 00:38:26 +0700

    17-04-2022 00.48

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,4 +1,4 @@
-import { getAllData, createData, updateData, deleteData, getDataById } from "../models/CategoryModel.js";
+import { Categories } from "../models/CategoryModel.js";
 import moment from "moment";
 import { v4 as uuidv4 } from 'uuid'; 
 
@@ -6,11 +6,15 @@ export async function getAllCategories(req,res){
 
     try{
 
-        let data = await getAllData.findAll({
+        let data = await Categories.findAll({
+
             where : {
+
                 "deleted_by" : null,
                 "deleted_at" : null
+
             }
+
         });
 
         let responseData = {
@@ -21,7 +25,7 @@ export async function getAllCategories(req,res){
 
         }
 
-        console.log("Success get data...");
+        // console.log("Success get data...");
 
         return res.status(res.statusCode).json(responseData);
 
@@ -50,9 +54,72 @@ export async function getCategoryById(req,res){
 
     let id = req.params.id;
 
-    let sql = `SELECT * FROM categories WHERE id='${id}'`;
+    try{
 
-    getDataById(res, sql);
+        let data = await Categories.findAll({
+
+            where : {
+
+                "id" : id,
+                "deleted_by" : null,
+                "deleted_at" : null
+
+            }
+
+        });
+
+        // console.log(data);
+
+        if(data.length < 1){
+
+            let responseData = {
+            
+                "success" : false,
+                "status" : 401,
+                "data" : data
+    
+            }
+    
+            console.log("Failed get data...");
+    
+            return res.status(401).json(responseData);
+
+
+        }else{
+
+            let responseData = {
+            
+                "success" : true,
+                "status" : res.statusCode,
+                "data" : data
+    
+            }
+    
+            console.log("Success get data...");
+    
+            return res.status(res.statusCode).json(responseData);
+
+        }
+
+
+    }catch(error){
+
+        console.log("Failed get data...");
+        
+        console.log(error);
+
+        let responseData = {
+        
+            "success" : false,
+            "status" : 401,
+            "message" : "Failed get data",
+            "data" : {}
+
+        }
+
+        return res.status(401).json(responseData);
+
+    }
 
 }
 
@@ -64,24 +131,62 @@ export async function createCategory(req,res){
     let timeNow = moment().format("YYYY-MM-DD hh:mm:ss");
     let user = 'system'
 
-    let sql = `INSERT INTO categories VALUES ('${id}','${name}', '${timeNow}', '${user}', '${timeNow}', '${user}', ${null}, ${null})`;
+    try{
 
-    //data for response if success
-    let data = {
+        //data for response if success
+        let data = {
 
-        "id" : id,
-        "name" : name,
-        "created_at" : timeNow,
-        "created_by" : user,
-        "updated_at" : timeNow,
-        "updated_by" : user,
-        "deleted_at" : null,
-        "deleted_by" : null,
+            "id" : id,
+            "name" : name,
+            "created_at" : timeNow,
+            "created_by" : user,
+            "updated_at" : timeNow,
+            "updated_by" : user,
+            "deleted_at" : null,
+            "deleted_by" : null,
+
+        }
+
+        await Categories.create(data);
+
+        let responseData = {
+            
+            "success" : true,
+            "status" : res.statusCode,
+            "message" : "Success create data",
+            "data" : data
+
+        }
+
+        // console.log("Success get data...");
+
+        return res.status(res.statusCode).json(responseData);
+
+    }catch(error){
+
+        console.log(error);
+
+        console.log("Failed create data category...");
+
+        let errorStr = JSON.stringify(error);
+
+        let errorToJson = JSON.parse(errorStr);
+
+        // console.log(errorToJson);
+
+        let errorObj = {
+
+            "error" : true,
+            "status" : 401,
+            "message" : errorToJson.original.sqlMessage,
+
+        }
+
+        // console.log(errorObj);
+
+        return res.status(401).json(errorObj);
 
     }
-
-    //model create
-    createData(res, sql, data);
 
 }
 
@@ -92,19 +197,68 @@ export async function updateCategory(req, res){
     let user = req.body.updated_by;
     let timeNow = moment().format("YYYY-MM-DD hh:mm:ss");
 
-    let sql = `UPDATE categories SET name='${name}',updated_at='${timeNow}', updated_by='${user}' WHERE id = '${id}' `;
 
-    //data for response if success
-    let data = {
+    try{
 
-        "id" : id,
-        "name" : name,
-        "updated_at" : timeNow,
-        "updated_by" : user,
+            //data for response if success
+        let data = {
+
+            "name" : name,
+            "updated_at" : timeNow,
+            "updated_by" : user,
+        }
+
+        // console.log(data);
+
+        await Categories.update(data, {
+
+            "where" : {
+
+                "id" : id
+
+            }
+
+        });
+
+        // console.log("Success udpate data category...");
+
+        let responseData = {
+            
+            "success" : true,
+            "status" : res.statusCode,
+            "message" : "Success udpate data",
+            "data" : data
+
+        }
+
+        // console.log(responseData);
+
+        return res.status(res.statusCode).json(responseData);
+
+
+    }catch(error){
+
+        console.log("Failed udpate data category...");
+
+        let errorStr = JSON.stringify(error);
+
+        let errorToJson = JSON.parse(errorStr);
+
+        // console.log(errorToJson);
+
+        let errorObj = {
+
+            "error" : true,
+            "status" : 401,
+            "message" : errorToJson.original.sqlMessage,
+
+        }
+
+        // console.log(errorObj);
+
+        return res.status(401).json(errorObj);
+
     }
-
-
-    updateData(res, sql, data);
 
 }
 
@@ -115,8 +269,63 @@ export async function deleteCategory(req, res){
     let user = req.body.deleted_by;
     let timeNow = moment().format("YYYY-MM-DD hh:mm:ss");
 
-    let sql = `UPDATE categories SET deleted_at='${timeNow}', deleted_by='${user}' WHERE id = '${id}' `;
-    
-    deleteData(res, sql);
+    try{
+
+        let data = {
+
+            "deleted_at" : timeNow,
+            "deleted_by" : user
+
+        }
+
+        // console.log(data);
+
+        await Categories.update(data, {
+
+            "where" : {
+
+                "id" : id
+
+            }
+
+        });
+        
+        // console.log("Success delete data category...");
+
+        let responseData = {
+
+            "success": true,
+            "status": res.statusCode,
+            "message": "Success delete data...",
+        }
+
+        return res.status(res.statusCode).json(responseData);
+
+    }catch(error){
+
+        console.log(error);
+
+        console.log("Failed delete data category...");
+
+        let errorStr = JSON.stringify(error);
+
+        let errorToJson = JSON.parse(errorStr);
+
+        // console.log(errorToJson);
+
+        let errorObj = {
+
+            "error" : true,
+            "status" : 401,
+            "message" : errorToJson.original.sqlMessage
+
+        }
+
+        // console.log(errorObj);
+
+        return res.status(401).json(errorObj);
+
+    }
+
 
 }
